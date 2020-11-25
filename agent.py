d378647c15c626276572d541df1891376e382af1
Jen <jenhoang304@gmail.com>
2020-11-25 04:05:44 +0000

    update manager and agent

--- agent.py
+++ agent.py
@@ -1,16 +1,56 @@
 #!/usr/bin/env python3
+import subprocess
+import asyncio 
 
 HOST = '127.0.0.1'
 PORT = 12345
 
-command_file = 'commands.conf'
+
+COMMAND_FILE = 'commands.conf'
+  
+def generate_command_list():
+    result = []
+    try:
+        with open(COMMAND_FILE, 'r') as f:
+            command_list = f.readlines()
+            result = list(map(lambda command: ' '.join(command.split()), command_list))
+            #gfprint(result) 
+
+    except Exception as e:    
+        print(e)
+    return result
+
+def is_valid_command(command):
+    is_valid = False
+    #print(generate_command_list())
+    if command.strip() in generate_command_list():
+        is_valid = True    
+    #print(is_valid)
+    return is_valid
+
+
+def run_command(command):
+    exec_command = command.split()
+    return subprocess.check_output(exec_command, encoding='utf-8')
 
 
 async def echo(reader, writer):
     try:
-        
+        command = await reader.readline()
+        #print(command)
+        command = command.decode('utf-8')
+        if(is_valid_command(command)):
+            output = run_command(command)
+            writer.write(output.encode('utf-8'))
+            await writer.drain()
+        else:
+            writer.write(b'Invalid command')
+            await writer.drain()
+
     except Exception as e:
         print(e)
+        writer.write(b'Unexpected error')
+        await writer.drain()
     finally:
         writer.close()
         await writer.wait_closed()
@@ -20,3 +60,4 @@ async def main():
     await server.serve_forever() # without this, program terminates
 
 asyncio.run(main()) 
+
--- manager.py
+++ manager.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python3
+import asyncio
+import sys
 
 PORT = 12345
+AGENTS_FILE = 'agents.conf'
+BUF_SIZE = 1024
 
+
+
+async def contact_agent(host, command):
+    reader, writer = await asyncio.open_connection(host, PORT)        
+    try:        
+        writer.write(command.encode('utf-8') + b'\n') 
+        await writer.drain()  
+        receive_msg = b''
+        while True:                        
+            next_data = await reader.read(n = BUF_SIZE)                        
+            if not next_data:                        
+                break                      
+            receive_msg = receive_msg + next_data
+        addr = writer.get_extra_info('peername')
+        print(f"Response from {addr}")
+        print(receive_msg.decode('utf-8') + '\n')   
+
+    except Exception as e:        
+        print(e)      
+          
+    finally:        
+        writer.close()      
+        await writer.wait_closed()
+
+async def run_manager(command):
+    print(f"Run command: {command}")
+    try:
+        with open(AGENTS_FILE, 'r') as f:
+            agent_list = f.readlines()
+            for agent_addr in agent_list:
+                print(agent_addr)
+                await contact_agent(agent_addr.strip(), command)
+    except Exception as e:    
+        print(e)
+
+
+while True:
+    request_command = input('Enter a command: \n')
+    if(request_command.strip() != ''):
+        break
+
+asyncio.run(run_manager(request_command))
