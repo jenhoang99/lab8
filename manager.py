4ae0f0d29780d326b2acd372be6da9cfe905124e
Jen <jenhoang304@gmail.com>
2020-11-27 19:24:32 +0000

    update comments

--- manager.py
+++ manager.py
@@ -7,11 +7,18 @@ BUF_SIZE = 1024
 
 async def get_msg(reader):
     '''
-    Purpose: receive the message from agent via reader and return that message
+    Purpose: 
+        receive the message from agent via reader and return that message in byte type
+
     Params:
         reader: An asyncio reader connected to the agent
+
     Return/Side Effect:
         receive_msg (byte) the message from agent via reader
+
+    Note:
+        the function does not handle exceptions
+        The system may be crashed if retrieved message is too big
 
     '''
     receive_msg = b''
@@ -25,14 +32,20 @@ async def get_msg(reader):
 
 async def contact_agent(command_key, host, port):
     '''
-    Purpose: * Contact a agent via host and port value 
-             * Send command_key to agent
-             * Receive result from agent and print on the terminal
+    Purpose: given command_key, host and port, the function will:
+        * Contact a agent via host and port values 
+        * Send 'command_key' to agent
+        * Receive result from agent and print it on the terminal
+
     Params:
         command_key (string) the key of the command to execute
-        host (agent) the IPv6 address of the agent to contact
-        port (int) the port number of agent to contact 
-    Note: As the message from agent my contains 
+        host (string) the IPv6 address of the agent to contact
+        port (string) the port number of agent to contact 
+
+    Note:
+        Exceptions will be catched in this function if error occurs. 
+            The error message will be printed on the terminal and connection will be then closed
+        Agent at host and port value should be up and running
     '''
     reader, writer = await asyncio.open_connection(host, port)        
     try:        
@@ -54,14 +67,19 @@ async def contact_agent(command_key, host, port):
 
 async def run_manager(command_key):
     '''
-    Purpose: Receive command key from user input, 
+    Purpose: Receive command key from user input, send the key to each agent and print the agent's response.
+    Work flow:
             * Get agent list from AGENTS_FILE
-            * Contact to each agent in the agents 
-            by calling contact_agent() function and pass the command key, agent'shost and agent's port to this function
+            * Contact to each agent in the agents by calling contact_agent() function 
+                and pass the command key, host and port of each agent to this function
+
     Params: command_key (string) message request will be sent to each agent from user input
-    Note:  AGENTS_FILE contains a list of agent address with IPv6 address and port, 
-            that are separated by a tab
-        => the function will split each line to assign to host and port variable
+
+    Note:  
+        AGENTS_FILE contains a list of agent address, 
+            each line has IPv6 adddress and port that are separated by a tab.
+        The function will handle exceptions. If error occurs, 
+            it will print the error message on the terminal.
 
     '''
     try:
